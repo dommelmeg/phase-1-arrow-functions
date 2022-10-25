8e05e7d52f3f12b8fd38cf9f2609a5aae19e2523
Megan <dommelmeg@gmail.com>
2022-10-24 22:40:17 -0400

    commit tree 14675382baf5239a73577a5067294df937d91850

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    let a = 2000;
+    let b = 100;
+    return a/b;
+}
+
+const square = parameter => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
